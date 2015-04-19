f71aee67b00e952a1bcc51a82c65a67814afcab6
Pisces000221 <1786762946@qq.com>
2015-04-19 16:37:58 +0800

    Get ready for flyers other than spacecrafts

--- src/class-flyers.js
+++ src/class-flyers.js
@@ -1,3 +1,16 @@
+// Move for one month.
+so.Flyer = {};
+so.Flyer.tick = function (newlyReachedTmp) { return function () {
+    var diffx = this.destx - this.x, diffy = this.desty - this.y;
+    var way2go = Math.sqrt(diffx * diffx + diffy * diffy);
+    if (way2go > this._lastWayToGo) {
+        newlyReachedTmp.push(this);
+    }
+    this.x += diffx / way2go * this.speed / 12;
+    this.y += diffy / way2go * this.speed / 12;
+    this._lastWayToGo = way2go;
+}; };
+
 so.Spacecraft = function (cosmos, civil, level, src, dest, callback, target) {
     var r = {
         cosmos: cosmos,
@@ -15,7 +28,7 @@ so.Spacecraft = function (cosmos, civil, level, src, dest, callback, target) {
         destSolarIdx: -1,   // Manual
         _lastWayToGo: 18906416
     };
-    r.tick = so.Spacecraft.tick;
+    r.tick = so.Flyer.tick(r.cosmos._newlyReached);
 
     return r;
 };
@@ -23,15 +36,3 @@ so.Spacecraft = function (cosmos, civil, level, src, dest, callback, target) {
 so.Spacecraft.speed = [undefined, 0.005, 0.015, 0.1, 1, 1];
 so.Spacecraft.warp = [undefined, undefined, undefined, undefined, 0.8, 0];
 so.Spacecraft.capacity = [undefined, 1500, 5000, 10000, 20000, 30000];
-
-// Move for one month.
-so.Spacecraft.tick = function () {
-    var diffx = this.destx - this.x, diffy = this.desty - this.y;
-    var way2go = Math.sqrt(diffx * diffx + diffy * diffy);
-    if (way2go > this._lastWayToGo) {
-        this.cosmos._newlyReached.push(this);
-    }
-    this.x += diffx / way2go * this.speed / 12;
-    this.y += diffy / way2go * this.speed / 12;
-    this._lastWayToGo = way2go;
-};
